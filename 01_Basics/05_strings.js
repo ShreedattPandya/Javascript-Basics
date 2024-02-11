2a0cc31a6be04d1b7b2d3980e3e9d0b223d01a2a
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-11 15:37:11 +0000

    String and it's methods

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,26 @@
+const fname = "shreedatt"
+const age = 20
+
+//console.log(fname + age); This is old js
+
+console.log(`My name is ${fname} and my age is ${age}`);//Updated Way
+
+const dynamic = new String('java')
+// console.log(dynamic[0]);  Gives string's 0th index value  
+// console.log(dynamic.__proto__); 
+// console.log(dynamic.length); Total length
+// console.log(dynamic.toUpperCase()); TO uppercase
+// console.log(dynamic.charAt(2)); Finds character at index 2
+// console.log(dynamic.indexOf('t')); finds the index of 2
+// console.log(dynamic.substring(0,5)); All char from 0 to 5
+// console.log(dynamic.slice(-2,-3)); 
+
+const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim()); //Removes extra spaces
+
+const url = "www.shreedatt%20pandya"
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('shreedatt')) //Checks if shreedatt is present
+const newStringTwo = "hitesh-two"
+console.log(newStringTwo.split('-')) //Divides in parts that is in quotation
