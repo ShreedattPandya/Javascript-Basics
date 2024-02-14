4d51f64777e8c8da92739d0fb4486225b61116b1
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-14 16:46:25 +0000

    Object-[Non]Singleton, Nested, Merge, Object in Array,some methods

--- 02_Arrays&Objects/04_object.js
+++ 02_Arrays&Objects/04_object.js
@@ -23,12 +23,14 @@ const obj1 = {1: "a", 2: "b"}
 const obj2 = {3: "a", 4: "b"}
 const obj4 = {5: "a", 6: "b"}
 
+
 // Merge Objects
+
 //==>1 Unprofessional way - Distributed
  //console.log(obj1, obj2,obj4); 
 
-//==>2 Without "{}" target(filled) will be obj1, else {} will be filled
-//const obj3 = Object.assign({},obj1,obj2,obj4) 
+//==>2 Object.assign
+//const obj3 = Object.assign({},obj1,obj2,obj4) // Without "{}" target(filled) will be obj1, else {} will be filled
 // console.log(obj3);
 
 //==>3 Spread method
@@ -46,8 +48,14 @@ const user = [
         name:"b"
     }
 ]
+// console.log(user[1].name); //Second element of array + name key of object
 
-console.log(user[1].name); //Second element of array + name key of object
+//Use object keys or value to make individual array respectively
+console.log(Object.keys(obj1));
+console.log(Object.values(obj1));
+console.log(Object.entries(obj1)); //[key:value]
+
+console.log(sing.hasOwnProperty('name')); //Key is present?
 
 
 
