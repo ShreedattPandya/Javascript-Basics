80aa481b299efb4bb7b6eaf4d9480952e2f9c04b
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-14 18:31:31 +0000

    Parameters in Functions

---
+++ 03_Function,Scopes,Arrow/01_functions.js
@@ -0,0 +1,17 @@
+function addTwoNumbers(number1,number2){
+    // let result = number1 + number2
+    // return result
+    return number1 + number2
+}
+const result = addTwoNumbers(5,7)
+console.log("Result: ", result);
+
+function loginUserMessage(username){ // OR username = "Ram"
+    if(!username){ //username === undefined
+        console.log("PLease enter a username");
+        return 
+    }
+    return `${username} just loggedin`
+    
+}
+console.log(loginUserMessage(""));
