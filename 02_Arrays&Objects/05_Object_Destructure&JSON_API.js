3fc420370c7c559c73fc574b56da2085c79a72e0
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-14 17:39:58 +0000

    Destruct Object, Create & format JSON file

---
+++ 02_Arrays&Objects/05_Object_Destructure&JSON_API.js
@@ -0,0 +1,29 @@
+const course = {
+    courseInstructor : "Hitesh",
+    platform : "YouTube",
+    price : 0
+}
+
+//Destructing Object - extracting property
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+// const {courseInstructor: instructor} = course //Made its short form-instructor
+// console.log(instructor);
+
+//Before JSON, xml file was used
+//JSON
+{
+    "name" : "Shyam"
+    "salary" : 50000,
+    "post" : "Manager"
+}
+
+//can also be in array
+// [
+//     {}
+//     {}
+// ]
+
+// randomuser.me provides readymade APIk
+// jsonformatter.org converts JSON file to readable format
