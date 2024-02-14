a4c16feda0f7cc406a795af8b046dd0a5bb5bae8
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-14 11:14:13 +0000

    Object-[Non]Singleton, Nested, Merge, Object in Array

---
+++ 02_Arrays&Objects/04_object.js
@@ -0,0 +1,54 @@
+
+//const sing = new Object() //Singleton Object [Through Constructor]
+
+const sing = {} //Non-Singleton [Through Litrals]
+sing.id = "123abc"
+sing.name = "Shreedatt"
+sing.isLoggedIn = false
+
+// console.log(sing);
+
+const nested = {
+    email : "nested@google.com",
+    emp:{
+        name:{
+            firstname : "nestedBro"
+        }
+    }
+
+}
+// console.log(nested.emp.name);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// Merge Objects
+//==>1 Unprofessional way - Distributed
+ //console.log(obj1, obj2,obj4); 
+
+//==>2 Without "{}" target(filled) will be obj1, else {} will be filled
+//const obj3 = Object.assign({},obj1,obj2,obj4) 
+// console.log(obj3);
+
+//==>3 Spread method
+const obj3 = {...obj1,...obj2,...obj4}
+// console.log(obj3);
+
+//==> Object in Array
+const user = [
+    {
+        id:123,
+        name:"a"
+    },
+    {
+        id:456,
+        name:"b"
+    }
+]
+
+console.log(user[1].name); //Second element of array + name key of object
+
+
+
+
