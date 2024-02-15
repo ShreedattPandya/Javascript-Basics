db78b9ac7a6c6d6f5ce4b8b5cd96cea261a19a85
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-15 08:42:13 +0000

    Unlimited Function Parameters, Function for Object, Array

---
+++ 03_Function,Scopes,Arrow/02_functions_B.js
@@ -0,0 +1,31 @@
+//Adding undefined number of parameters values
+function calculateCartPrice(...num){  //(val1, val2, ...num1)
+    return num
+}
+// console.log(calculateCartPrice(10,20,30));
+
+// Funtions for Object
+const user = {
+    name : "Shreedatt",
+    price : 199
+}
+
+function objectFunction(anyObject){
+    console.log(`Username is ${anyObject.name} and price ${anyObject.price}`)
+}
+
+// objectFunction(user);
+// objectFunction({
+//     name: "sam",
+//     price: 399
+// })
+
+const Arr = [200,400,600,800]
+
+//Function for Array
+function arrayFunction(anyArray){
+    // return anyArray[2]   
+    return anyArray.slice(0,2)
+}
+//console.log(arrayFunction(Arr));
+console.log(arrayFunction([100,200,300]));
