d9303768f5317b2c08c489ac135408060e7cdb1a
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-16 08:42:21 +0000

    Scope of a block, Hoisting

--- 03_Function,Scopes,Arrow/03_scopes.js
+++ 03_Function,Scopes,Arrow/03_scopes.js
@@ -10,4 +10,25 @@ if (true) {
 // console.log(b);
 // console.log(c);
 
- 
+
+if(true){
+    const username = "anyName"
+    if(username === "anyName"){
+        const website = "youTube"
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+console.log(addone(5)) //Function without expression(INSIDE VARIBLE) can be called before declaration
+function addone(num){
+    return num + 1
+}
+
+addTwo(5) //expression(function inside variable) can't be called before declaration
+const addTwo = function(num){
+    return num +2
+}
+
+// NOTE: function initialization can be done before it's declaration if its not inside variable(expression). This is Hoisting.  
