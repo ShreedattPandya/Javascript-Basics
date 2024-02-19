985e75d212ed2be97c93e304599756a37f500da8
ShreedattPandya <159604439+ShreedattPandya@users.noreply.github.com>
2024-02-19 10:36:31 +0000

    while, do-while loop

---
+++ 05_iterations/02_while.js
@@ -0,0 +1,17 @@
+//while loop
+
+let score = 0
+
+while (score <= 10){
+    console.log(`The updated score is ${score}`);
+    score += 2;
+}
+
+//do-while
+
+let price = 20
+do{
+    console.log(`Buy it for ${price}`);
+    price--
+}
+while(price <= 10)
